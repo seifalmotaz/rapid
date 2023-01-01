330e6f391c25a101ba112f184fae19ee793bec48
Seif Almotaz Bellah <seifalmotaz@protonmail.com>
2023-01-01 23:01:23 +0200

    dirs change

--- rapid/lib/rapid.dart
+++ rapid/lib/rapid.dart
@@ -4,6 +4,6 @@ export './src/app.dart' show RapidApp, RapidRoutes;
 export './src/response_writer/writer.dart';
 
 export './src/context/context.dart' show Context;
-export './src/context/form_data.dart' show FormData, FilePart;
+export './src/context/form_data.dart' show BodyForm, FilePart;
 
 export './src/utils/mimetypes.dart' show MimeType;
