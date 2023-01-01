71d438143e3f3dbac8539049bf2152cadf15e0d0
Seif Almotaz Bellah <seifalmotaz@protonmail.com>
2023-01-01 23:48:37 +0200

    step two in the project

---
+++ aka_rapid/example/aka_rapid_example.dart
@@ -0,0 +1,6 @@
+import 'package:aka_rapid/aka_rapid.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ aka_rapid/test/aka_rapid_test.dart
@@ -0,0 +1,16 @@
+import 'package:aka_rapid/aka_rapid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
